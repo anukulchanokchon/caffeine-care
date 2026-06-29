a98f39c0ccfa48c4a6cdaa8f0b46fe03cc3f2ea3
anukulchanokchon <anukulchanokchon@gmail.com>
2026-06-29 22:43:07 +0700

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCgO3cahh5Q0DxETWMxAZhDB8-kGnXW93U",
+  authDomain: "caffeine-care.firebaseapp.com",
+  projectId: "caffeine-care",
+  storageBucket: "caffeine-care.firebasestorage.app",
+  messagingSenderId: "579804259480",
+  appId: "1:579804259480:web:7382cf3d4347a5961c9ced",
+  measurementId: "G-ENXCYBD1SL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Authentication
+const auth = getAuth(app);
+
+// ทำให้ไฟล์อื่นเรียกใช้ได้
+window.auth = auth;
+
+console.log("Firebase Connected ✅");
